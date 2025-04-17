568efad3067965a0062313b19e91341d64aa1220
Ruchir Kalokhe <ruchirkalokhe@gmail.com>
2025-04-17 14:28:08 +0000

    cofig updated to use secrets.toml from .env file

--- config/config.py
+++ config/config.py
@@ -1,15 +1,13 @@
 # config/config.py
 import os
-from dotenv import load_dotenv  # type: ignore
+import streamlit as st
 
-# Load environment variables
-load_dotenv()
+# No need for load_dotenv() as we're using Streamlit secrets
 
 class Config:
-    MONGODB_URI = os.getenv('MONGODB_URI', 'mongodb://localhost:27017/')
-    DB_NAME = os.getenv('DB_NAME', 'healthcare_platform')
+    MONGODB_URI = st.secrets.get('MONGODB_URI', 'mongodb://localhost:27017/')
+    DB_NAME = st.secrets.get('DB_NAME', 'healthcare_platform')
     
-   
     PUBMEDBERT_MODEL = "microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext"
     CLINICALBERT_MODEL = "emilyalsentzer/Bio_ClinicalBERT"
     MEDICAL_NER_MODEL = "samrawal/bert-base-uncased_clinical-ner"
@@ -18,9 +16,9 @@ class Config:
     TEXT_GENERATION_MODEL = "./models/PegasusXSum"
     TEXT_GENERATION_TOKENIZER = "./models/PegasusXSum_tokenizer"
     
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = st.secrets.get('SECRET_KEY')
     
-    EMERGENCY_PHONE = os.getenv('EMERGENCY_PHONE', '102')
+    EMERGENCY_PHONE = st.secrets.get('EMERGENCY_PHONE', '102')
     
     SESSION_LIFETIME = 24 * 60 * 60  # 24 hours in seconds
     
